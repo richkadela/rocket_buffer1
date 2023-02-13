b3bc070edf247699d64264a3edf882d6e3850f94
Rich <123497822+richkadela@users.noreply.github.com>
2023-02-13 13:36:20 -0300

    Add files via upload

---
+++ src/core/core/interface.lua
@@ -0,0 +1,7 @@
+Um abraço pro professor frexi
+Um abraço pra toda equipe roqueti pra n dizer que sou desulmilde >)
+Eu sou lindo :)
+Vcs viram que eu manjo nos iscripiti ne ? Pega nada pae 
+Quer aprender ? Arrasta pra cima.
+
+ALEGRIAAAA SENHORES.
